21cc6edc500a95ac7d6835af5307081c74a68f1f
juan-josue <juansemail2001@gmail.com>
2023-04-08 17:27:22 -0400

    feat: Create project model in models folder

---
+++ tracktion-api/models/project.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+	},
+	level: {
+		type: Number,
+		default: 0,
+	},
+	xp: {
+		type: Number,
+		default: 0,
+	},
+});
+
+const projectSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+	},
+	members: [memberSchema],
+	tasks: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Task',
+		},
+	],
+});
+
+const Project = mongoose.model('Project', projectSchema);
+exports.Project = Project;
