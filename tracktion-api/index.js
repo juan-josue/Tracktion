fe3c9f14e602badc3d5cb85ac5728e42d31516cf
juan-josue <juansemail2001@gmail.com>
2023-04-08 14:52:46 -0400

    Initial commit: Add project files and server setup

---
+++ tracktion-api/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+
+app.use(morgan('tiny'));
+
+// Define the port to listen on
+const port = process.env.PORT || 3000;
+
+// Define database uri (uniform resource identifier)
+const db = process.env.DB_URI;
+
+mongoose
+	.connect(db)
+	.then(() => console.log('Connected to MongoDB Atlas...'))
+	.catch((err) => console.error('Could not connect to MongoDB Atlas...', err));
+
+// Start listening on the specified port
+app.listen(port, () => {
+	console.log(`Listening on port ${port}...`);
+});
