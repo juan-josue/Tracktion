db6ad61b493dd9265fc2d3cd8f0b73a33c8cae9b
juan-josue <juansemail2001@gmail.com>
2023-04-13 21:40:16 -0400

    fix: rework project member add and delete routes

--- tracktion-api/routes/projects.js
+++ tracktion-api/routes/projects.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const { Project, generateJoinCode } = require('../models/project');
 const { Member } = require('../models/member');
+const { User } = require('../models/user');
 const router = express.Router();
 
 // GET specified project
@@ -27,30 +28,6 @@ router.post('/', async (req, res) => {
 	res.send(project);
 });
 
-// POST a new project member
-router.post('/:id/members', async (req, res) => {
-	let project = await Project.findById(req.params.id);
-	if (!project)
-		return res.status(404).send('The project with the given ID was not found.');
-
-	const existingMember = await Member.findOne({
-		project: req.params.id,
-		user: req.body.userId,
-	});
-	if (existingMember)
-		return res.status(400).send('The user is already a member for this project.');
-
-	let newMember = new Member({
-		user: req.body.userId,
-		project: req.params.id,
-	});
-	newMember = await newMember.save();
-
-	project.members.push(newMember._id);
-	project = await project.save();
-	res.send(project);
-});
-
 // PUT specified project
 router.put('/:id', async (req, res) => {
 	const project = await Project.findByIdAndUpdate(
@@ -71,6 +48,57 @@ router.delete('/:id', async (req, res) => {
 		return res.status(404).send('The project with the given ID was not found.');
 
 	res.send(project);
+});
+
+// POST a new project member
+router.post('/members', async (req, res) => {
+	let project = await Project.findOne({ code: req.body.joinCode });
+	if (!project)
+		return res.status(404).send('A project with the given join code was not found.');
+
+	const existingMember = await Member.findOne({
+		project: project._id,
+		user: req.body.userId,
+	});
+	if (existingMember)
+		return res.status(400).send('The user is already a member for this project.');
+
+	let newMember = new Member({
+		user: req.body.userId,
+		project: project._id,
+	});
+	newMember = await newMember.save();
+
+	const user = await User.findById(newMember.user);
+	if (!user) return res.status(404).send('The user with the given ID was not found.');
+	console.log(user.projects);
+	user.projects.push(project._id);
+	await user.save();
+
+	project.members.push(newMember._id);
+	await project.save();
+
+	res.send(newMember);
+});
+
+// DELETE a member
+router.delete('/members/:id', async (req, res) => {
+	const member = await Member.findByIdAndDelete(req.params.id);
+	if (!member)
+		return res.status(404).send('The member with the given ID was not found.');
+
+	const project = await Project.findById(member.project);
+	if (!project)
+		return res.status(404).send('The project with the given ID was not found.');
+	project.members.pull(member._id);
+	await project.save();
+
+	const user = await User.findById(member.user);
+	if (!user) return res.status(404).send('The user with the given ID was not found.');
+	user.projects.pull(project._id);
+	await user.save();
+
+	res.send(member);
 });
 
 module.exports = router;
