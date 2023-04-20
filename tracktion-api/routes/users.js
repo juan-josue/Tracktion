751e4bec1747a53b9d460324311f6b9b769b64d4
juan-josue <juansemail2001@gmail.com>
2023-04-19 23:39:47 -0400

    feat: Add hashed user passwords with bcrypt

--- tracktion-api/routes/users.js
+++ tracktion-api/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const express = require('express');
 const { User, validateUser } = require('../models/user');
@@ -21,13 +22,25 @@ router.post('/', async (req, res) => {
 	const { error } = validateUser(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
-	let user = new User({
+	let user = await User.findOne({ email: req.body.email });
+	if (user) return res.status(400).send('User already registered.');
+
+	user = new User({
 		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password,
 	});
-	user = await user.save();
-	res.send(user);
+
+	const salt = await bcrypt.genSalt(10);
+	user.password = await bcrypt.hash(user.password, salt);
+	await user.save();
+
+	res.send({
+		_id: user._id,
+		name: user.name,
+		email: user.email,
+		projects: user.projects,
+	});
 });
 
 // PUT specified user
