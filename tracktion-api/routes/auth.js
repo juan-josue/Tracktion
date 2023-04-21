854ae19950d6c938f3c31e6bb11c487f3e50b760
juan-josue <juansemail2001@gmail.com>
2023-04-20 23:59:52 -0400

    feat: Add jwt token

--- tracktion-api/routes/auth.js
+++ tracktion-api/routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
@@ -16,7 +17,8 @@ router.post('/', async (req, res) => {
 	const validPassword = await bcrypt.compare(req.body.password, user.password);
 	if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-	res.send(true);
+    const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+	res.send(token);
 });
 
 function validate(req) {
