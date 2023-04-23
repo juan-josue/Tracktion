16f65e84715b35526c9a1bf189a93e4891cd2ed1
juan-josue <juansemail2001@gmail.com>
2023-04-23 02:09:52 -0400

    feat: Add material ui and basic login layout

--- tracktion-frontend/src/App.tsx
+++ tracktion-frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Grid } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid container height='100vh'>
+      <Grid item xs={12} sm={6} sx={{ height: { xs: '70%', sm: '100%' } }}>
+        <Box sx={{ bgcolor: 'primary.light', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Box bgcolor="primary.main" p={2}>
+            Login
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ height: { xs: '30%', sm: '100%' } }}>
+        <Box sx={{ bgcolor: 'primary.dark', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Box bgcolor="primary.main" p={2}>
+            Picture
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
