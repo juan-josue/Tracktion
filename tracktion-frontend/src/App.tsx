38999c08edbb07f24c3742d9d2afd8f4dc77dd2b
juan-josue <juansemail2001@gmail.com>
2023-04-23 21:37:08 -0400

    feat: Add LoginForm component with Axios API call

--- tracktion-frontend/src/App.tsx
+++ tracktion-frontend/src/App.tsx
@@ -1,60 +1,39 @@
-import { Box, Button, Grid, Stack, TextField, Typography } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import LoginForm from './components/LoginForm';
 
 function App() {
 	return (
-		<Box height="100vh">
-			<Grid container height="100%">
-				<Grid item xs={12} md={6} sx={{ height: { xs: '70%', md: '100%' } }}>
-					<Box
-						sx={{
-							bgcolor: 'primary.light',
-							height: '100%',
-							display: 'flex',
-							flexDirection: 'column',
-							alignItems: 'center',
-							justifyContent: 'center',
-						}}
-					>
-						<Stack direction="column" spacing={1} mb={8} sx={{ width: '60%' }}>
-							<Typography variant="h4">Hey, Welcome Back to Tracktion!</Typography>
-							<Typography variant="body1">You can sign into your account here</Typography>
-						</Stack>
-						<Stack direction="column" spacing={3} mb={6} sx={{ width: '60%' }}>
-							<TextField label="Email" variant="filled">
-								Email
-							</TextField>
-							<TextField label="Password" variant="filled" type="password">
-								Password
-							</TextField>
-							<Button variant="contained" type="submit" sx={{ width: '50%' }}>
-								Sign In
-							</Button>
-						</Stack>
-						<Typography variant="body1">
-							Don't have an account?{' '}
-							<Box component="span" color="purple">
-								Sign up
-							</Box>
-						</Typography>
-					</Box>
-				</Grid>
-				<Grid
-					item
-					xs={12}
-					md={6}
-					p={4}
-					sx={{ height: { xs: '30%', md: '100%' }, paddingBottom: { xs: 0, md: 4 } }}
+		<Grid container height="100vh">
+			<Grid item xs={12} md={6} sx={{ height: { xs: '70%', md: '100%' } }}>
+				<Box
+					sx={{
+						bgcolor: 'primary.light',
+						height: '100%',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
 				>
-					<Box
-						sx={{
-							bgcolor: 'tomato',
-							height: '100%',
-							borderRadius: { xs: '15px 15px 0 0', md: '15px' },
-						}}
-					></Box>
-				</Grid>
+					<LoginForm />
+				</Box>
 			</Grid>
-		</Box>
+			<Grid
+				item
+				xs={12}
+				md={6}
+				p={4}
+				sx={{ height: { xs: '30%', md: '100%' }, paddingBottom: { xs: 0, md: 4 } }}
+			>
+				<Box
+					sx={{
+						bgcolor: 'tomato',
+						height: '100%',
+						borderRadius: { xs: '15px 15px 0 0', md: '15px' },
+					}}
+				></Box>
+			</Grid>
+		</Grid>
 	);
 }
 
---
+++ tracktion-frontend/src/components/LoginForm.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+const LoginForm = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		try {
+            const response = await axios.post('http://localhost:3000/api/auth', {
+              email,
+              password,
+            });
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+    };
+
+	return (
+		<>
+			<Stack direction="column" spacing={1} mb={8} sx={{ width: '60%' }}>
+				<Typography variant="h4">Hey, Welcome Back to Tracktion!</Typography>
+				<Typography variant="body1">You can sign into your account here</Typography>
+			</Stack>
+			<form onSubmit={handleSubmit} style={{ width: '60%' }}>
+				<Stack direction="column" spacing={3} mb={6}>
+					<TextField
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						label="Email"
+						variant="filled"
+						type="email"
+						required
+					>
+						Email
+					</TextField>
+					<TextField
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						label="Password"
+						variant="filled"
+						type="password"
+						required
+					>
+						Password
+					</TextField>
+					<Button variant="contained" type="submit" sx={{ width: '50%' }}>
+						Sign In
+					</Button>
+				</Stack>
+			</form>
+			<Typography variant="body1">
+				Don't have an account?
+				<Box component="span" color="purple">
+					Sign up
+				</Box>
+			</Typography>
+		</>
+	);
+};
+
+export default LoginForm;
