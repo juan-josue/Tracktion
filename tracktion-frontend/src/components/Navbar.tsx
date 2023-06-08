747e1b8f05f441ee7d945c09ada9a64809c37f79
juan-josue <juansemail2001@gmail.com>
2023-06-07 21:22:28 -0400

    feat: Add navigation functions to Navbar component

--- tracktion-frontend/src/components/Navbar.tsx
+++ tracktion-frontend/src/components/Navbar.tsx
@@ -2,19 +2,23 @@ import { Chip, IconButton, Stack } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonIcon from '@mui/icons-material/Person';
 import LogoutIcon from '@mui/icons-material/Logout';
-import logo from '../assets/logos/t.svg'
+import { useNavigate } from 'react-router-dom';
+
+import logo from '../assets/logos/t.svg';
 
 const Navbar = () => {
+	const navigate = useNavigate();
+
 	return (
-		<Stack px={2} bgcolor='bg.main' direction="row" justifyContent='space-between' alignItems="center" borderRadius="15px">
-			<Stack direction='row' spacing={2} alignItems="center">
-				<img src={logo} alt='Tracktion Logo' height='25px'></img>
-        <Chip color="primary" icon={<DashboardIcon />} label="Projects" />
-				<IconButton aria-label="Profile">
+		<Stack px={2} bgcolor="bg.main" direction="row" justifyContent="space-between" alignItems="center" borderRadius="15px">
+			<Stack direction="row" spacing={2} alignItems="center">
+				<img src={logo} alt="Tracktion Logo" height="25px"></img>
+				<Chip color="primary" icon={<DashboardIcon />} onClick={() => navigate('/projects')} label="Projects" />
+				<IconButton aria-label="Profile" onClick={() => navigate('/projects')}>
 					<PersonIcon />
 				</IconButton>
 			</Stack>
-			<IconButton aria-label="Logout">
+			<IconButton aria-label="Logout" onClick={() => navigate('/login')}>
 				<LogoutIcon />
 			</IconButton>
 		</Stack>
