7a76c6f06b70dee274aae52258f30dd28c055d34
juan-josue <juansemail2001@gmail.com>
2023-06-11 16:45:47 -0400

    feat: Add useUser hook to get the logged in user

---
+++ tracktion-frontend/src/hooks/useUser.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import apiClient from "../services/apiClient";
+import refreshAccessToken from "../services/refreshAccessToken";
+import { User } from "../types/types";
+
+const useProjects = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+		const accessToken = localStorage.getItem('access_token');
+
+		apiClient
+			.get('/users/me', { headers: { Authorization: `Bearer ${accessToken}` } })
+			.then((res) => setUser(res.data.user))
+			.catch(async (err) => {
+				if (err.response.status === 401) {
+					const newAccessToken = await refreshAccessToken();
+					if (newAccessToken) {
+						localStorage.setItem('access_token', newAccessToken);
+						apiClient
+							.get('/users/me', { headers: { Authorization: `Bearer ${newAccessToken}` } })
+							.then((res) => setUser(res.data.user))
+							.catch(() => navigate('/login'));
+					} else {
+						navigate('/login');
+					}
+				} else {
+					navigate('/login');
+				}
+			});
+	}, [navigate]);
+
+    return { user };
+};
+
+export default useProjects;
--- tracktion-frontend/src/pages/Projects/index.tsx
+++ tracktion-frontend/src/pages/Projects/index.tsx
@@ -13,35 +13,13 @@ import ProjectSelection from './ProjectSelection';
 import NewProjectForm from './NewProjectForm';
 import JoinProjectForm from './JoinProjectForm';
 import Modal from '../../components/Modal';
+import useUser from '../../hooks/useUser';
 
 const Projects = () => {
-	const [user, setUser] = useState<User | null>(null);
 	const [selectedProject, setSelectedProject] = useState<Project | null>(null);
 	const navigate = useNavigate();
 
-	useEffect(() => {
-		const accessToken = localStorage.getItem('access_token');
-
-		apiClient
-			.get('/users/me', { headers: { Authorization: `Bearer ${accessToken}` } })
-			.then((res) => setUser(res.data.user))
-			.catch(async (err) => {
-				if (err.response.status === 401) {
-					const newAccessToken = await refreshAccessToken();
-					if (newAccessToken) {
-						localStorage.setItem('access_token', newAccessToken);
-						apiClient
-							.get('/users/me', { headers: { Authorization: `Bearer ${newAccessToken}` } })
-							.then((res) => setUser(res.data.user))
-							.catch(() => navigate('/login'));
-					} else {
-						navigate('/login');
-					}
-				} else {
-					navigate('/login');
-				}
-			});
-	}, [navigate]);
+	const { user } = useUser();
 
 	const handleProjectCardClick = (project: Project) => {
 		setSelectedProject(project);
