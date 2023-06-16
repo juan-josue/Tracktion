5de0214c37e6759b128bc725f541aec356816389
juan-josue <juansemail2001@gmail.com>
2023-06-15 22:40:56 -0400

    feat: Add project edit and delete buttons only visible to owner

--- tracktion-frontend/src/pages/Projects/ProjectSelection.tsx
+++ tracktion-frontend/src/pages/Projects/ProjectSelection.tsx
@@ -1,5 +1,7 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import PlayIcon from '@mui/icons-material/PlayCircle';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import ClearIcon from '@mui/icons-material/Clear';
 import { useNavigate } from 'react-router-dom';
 
 import ProjectCard from './ProjectCard';
@@ -14,7 +16,7 @@ const ProjectSelection = ({ selectedProject, userId }: Props) => {
 	const navigate = useNavigate();
 
 	const handleClick = () => {
-		navigate('/tasks',  { state: { projectId: selectedProject?._id, userId: userId } });
+		navigate('/tasks', { state: { projectId: selectedProject?._id, userId: userId } });
 	};
 
 	if (!selectedProject) {
@@ -25,27 +27,41 @@ const ProjectSelection = ({ selectedProject, userId }: Props) => {
 		);
 	}
 
+	console.log(selectedProject);
+
 	return (
 		<>
-			<Typography variant="h4" color="typography.main">
-				Current selection
+			<Typography variant="h5" color="typography.main" mb={2}>
+				Current Selection
 			</Typography>
-			<Box my={3} display="flex" justifyContent="center">
+			<Box mb={2} display="flex" justifyContent="center">
 				<ProjectCard color="primary.light" project={selectedProject} />
 			</Box>
-			<Box display={{ xs: 'none', md: 'block' }}>
-				<Typography variant="h4" color="typography.main">
-					Project description
+
+			<Stack direction="row" mb={2} spacing={2} justifyContent="center">
+				<Button variant="contained" color="secondary" onClick={handleClick} endIcon={<PlayIcon />}>
+					Play
+				</Button>
+				{userId === selectedProject.owner && (
+					<Stack direction="row" mb={2} spacing={2} justifyContent="center">
+						<Button variant="contained" color="secondary" onClick={handleClick} endIcon={<ModeEditIcon />}>
+							Edit
+						</Button>
+						<Button variant="contained" color="secondary" onClick={handleClick} endIcon={<ClearIcon />}>
+							Delete
+						</Button>
+					</Stack>
+				)}
+			</Stack>
+
+			<Stack spacing={2} display={{ xs: 'none', md: 'block' }}>
+				<Typography variant="h5" color="typography.main">
+					Description
 				</Typography>
 				<Typography variant="body1" color="typography.main">
 					{selectedProject.description}
 				</Typography>
-			</Box>
-			<Box display="flex" justifyContent={{ xs: 'center', md: 'left' }} mt={2}>
-				<Button variant="contained" color="secondary" size="large" onClick={handleClick} endIcon={<PlayIcon />}>
-					Play Project
-				</Button>
-			</Box>
+			</Stack>
 		</>
 	);
 };
