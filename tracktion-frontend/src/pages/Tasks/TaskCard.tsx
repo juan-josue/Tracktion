700dc945c292f1e2451951bf2bae3c4be8db3859
juan-josue <juansemail2001@gmail.com>
2023-06-22 21:38:51 -0400

    feat: Add DeleteTaskForm component

--- tracktion-frontend/src/pages/Tasks/TaskCard.tsx
+++ tracktion-frontend/src/pages/Tasks/TaskCard.tsx
@@ -3,15 +3,12 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import CloseIcon from '@mui/icons-material/Close';
 import EditIcon from '@mui/icons-material/Edit';
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 
 import { Member, Task } from '../../types/types';
-import apiClient from '../../services/apiClient';
-import refreshAccessToken from '../../services/refreshAccessToken';
 import Modal from '../../components/Modal';
 import EditTaskForm from './EditTaskForm';
 import CompleteTaskModal from './CompleteTaskModal';
+import DeleteTaskForm from './DeleteTaskForm';
 
 interface Props {
 	members: Member[];
@@ -19,13 +16,6 @@ interface Props {
 }
 
 const TaskCard = ({ members, task }: Props) => {
-	const navigate = useNavigate();
-	const [errorMessage, setErrorMessage] = useState('');
-
-	function refreshPage() {
-		window.location.reload();
-	}
-
 	function addZeroPadding(idNumber: number) {
 		const idLength = 4;
 		if (idNumber.toString().length >= idLength) return idNumber.toString();
@@ -35,34 +25,6 @@ const TaskCard = ({ members, task }: Props) => {
 
 		return padding + idNumber.toString();
 	}
-
-	const deleteTask = () => {
-		if (!confirm('Are you sure you want to delete this Quest?')) {
-			return;
-		}
-
-		const accessToken = localStorage.getItem('access_token');
-
-		apiClient
-			.delete(`/tasks/${task._id}`, { headers: { Authorization: `Bearer ${accessToken}` } })
-			.then(() => refreshPage())
-			.catch(async (err) => {
-				if (err.response.status === 401) {
-					const newAccessToken = await refreshAccessToken();
-					if (newAccessToken) {
-						localStorage.setItem('access_token', newAccessToken);
-						apiClient
-							.delete(`/tasks/${task._id}`, { headers: { Authorization: `Bearer ${newAccessToken}` } })
-							.then(() => refreshPage())
-							.catch((err) => setErrorMessage(err.response.data));
-					} else {
-						navigate('/projects');
-					}
-				} else {
-					setErrorMessage(err.response.data);
-				}
-			});
-	};
 
 	return (
 		<Box borderRadius="15px" bgcolor="#24202e" border={task.status === 'Done' ? '3px solid #826b9e' : '3px solid white'}>
@@ -77,9 +39,17 @@ const TaskCard = ({ members, task }: Props) => {
 				justifyContent="center"
 				alignItems="flex-end"
 			>
-				<IconButton onClick={deleteTask} sx={{ position: 'absolute', top: 5, right: 5 }}>
+				{/* <IconButton onClick={deleteTask} sx={{ position: 'absolute', top: 5, right: 5 }}>
 					<CloseIcon fontSize="medium" />
-				</IconButton>
+				</IconButton> */}
+				<Modal
+					button={
+						<IconButton sx={{ position: 'absolute', top: 5, right: 5 }}>
+							<CloseIcon fontSize="medium" />
+						</IconButton>
+					}
+					content={<DeleteTaskForm task={task}/>}
+				/>
 				{task.status !== 'Done' && (
 					<>
 						<Modal
@@ -105,11 +75,6 @@ const TaskCard = ({ members, task }: Props) => {
 				)}
 			</Box>
 			<Stack direction="column" spacing={1} p={2}>
-				{errorMessage && (
-					<Typography variant="body1" color="error">
-						{errorMessage}
-					</Typography>
-				)}
 				<Box>
 					<Typography variant="h5">{task.name}</Typography>
 					<Typography variant="body1">{addZeroPadding(task.taskNumber)}</Typography>
