4d1abe64fc8f94c9d52af5c8d8f1ed240fdada89
juan-josue <juansemail2001@gmail.com>
2023-07-01 15:06:19 -0400

    feat: Add comments to middleware functions

--- tracktion-api/middleware/auth.js
+++ tracktion-api/middleware/auth.js
@@ -1,13 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+	// Get token from the authorization header
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
 	if (!token) return res.status(401).send('Access denied. No token provided.');
 
+	// Verify the token using the secret environment key
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
 		if (err) return res.status(401).send('Access denied. Invalid token provided.');
+
+		// If there were no errors set the req user to the decoded user
 		req.user = user;
+
+		// Pass control to the next middleware function
 		next();
 	});
 }
