5e3298ce6eb1bdbd4548d5fc8b9691001a9e161c
juan-josue <juansemail2001@gmail.com>
2023-07-02 02:46:41 -0400

    feat: Add comments to the user route

--- tracktion-api/routes/users.js
+++ tracktion-api/routes/users.js
@@ -7,26 +7,35 @@ const router = express.Router();
 
 // GET current user
 router.get('/me', auth, async (req, res) => {
+	// Find the user with the id provided by authentication middleware
 	const user = await User.findById(req.user._id).select('-password');
 	if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+	// Send the user in the response
 	res.send(user);
 });
 
 // GET specified user
 router.get('/:id', async (req, res) => {
+	// Find the user with the provided id
 	const user = await User.findById(req.params.id).select('-password');
 	if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+	// Send the user in the response
 	res.send(user);
 });
 
 // POST new user
 router.post('/', async (req, res) => {
+	// Validate the request body with the validateUser function
 	const { error } = validateUser(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
+	// Determine if a user already exists with the provided email
 	let user = await User.findOne({ email: req.body.email });
 	if (user) return res.status(400).send('An account with that email already exists.');
 
+	// Create a new user with the provided data
 	user = new User({
 		name: req.body.name,
 		email: req.body.email,
@@ -34,10 +43,14 @@ router.post('/', async (req, res) => {
 		pfp: req.body.pfp,
 	});
 
+	// Hash the provided password with bcrypt
 	const salt = await bcrypt.genSalt(10);
 	user.password = await bcrypt.hash(user.password, salt);
+
+	// Save the new user object to the database
 	await user.save();
 
+	// Send the user (without the password) in the response
 	res.send({
 		_id: user._id,
 		name: user.name,
@@ -49,21 +62,27 @@ router.post('/', async (req, res) => {
 
 // PUT specified user
 router.put('/:id', async (req, res) => {
+	// Validate the request body with the validateUser function
 	const { error } = validateUser(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
+	// Find the user with the given id and update their name with the provided data
 	const user = await User.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true }).select('-password');
 	if (!user) return res.status(404).send('The user with the given ID was not found.');
 
+	// Send the updated user in the response
 	res.send(user);
 });
 
 // DELETE specified user
 router.delete('/:id', async (req, res) => {
+	// Find and delete the user with the provided id
 	const user = await User.findByIdAndDelete(req.params.id).select('-password');
 	if (!user) return res.status(404).send('The user with the given ID was not found.');
 
+	// Send the deleted user in the response
 	res.send(user);
 });
 
+// Export the router object
 module.exports = router;
